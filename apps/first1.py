d93ba14666fdf24c65fbaa7ca784cbdfac11d32b
LEEGIYEONG <dnjsrhr775@gmail.com>
2021-10-13 02:58:37 +0900

    Add files via upload

---
+++ apps/first1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def app():
+    st.title('Home')
+
+    st.write('메인 홈페이지입니다')
+
+    st.write('LSTM모델을 사용하여 KOSPI 주가지수의 종가를 예측한다')
+
+    st.write('NeuralProphet모델을 사용하여 KOSPI 주가지수의 종가를 예측한다')
+
+    st.write('BERT 자연어 모델을 통한 경제뉴스 감성분류하여 주가지수를 예측한다')
+
+    st.write('본 연구는 과학기술정보통신부 및 정보통신기획평가원의 SW중심대학지원사업의 연구결과로 수행되었음(2018001874004).')
---
+++ apps/third1.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from datetime import date
+import pandas as pd
+import pandas_datareader as data
+import yfinance as yf
+import pickle
+import numpy as np
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+import matplotlib.pyplot as plot
+from neuralprophet import NeuralProphet
+from neuralprophet import set_random_seed
+from sklearn.metrics import mean_squared_error
+
+def app():
+    global data
+    global st
+    START = '2000-01-01'
+    TODAY = date.today().strftime('%Y-%m-%d')
+
+    user_input = st.text_input('Enter Stock Ticker', '^KS11')
+    df = data.DataReader(user_input,"yahoo", START, TODAY)
+
+    data = df.reset_index()
+    prcp_data = data.rename(columns={'Date': 'ds', 'Close': 'y'})[['ds', 'y']]
+    
+    st.subheader('Data from 2000 - 2021')
+    st.write(df.describe())
+
+    model = NeuralProphet(n_changepoints=100,
+                      trend_reg=0.05,
+                      yearly_seasonality=False,
+                      weekly_seasonality=False,
+                      daily_seasonality=False)
+    metrics = model.fit(prcp_data, validate_each_epoch=True, 
+                    valid_p=0.2, freq='D', 
+                    plot_live_loss=True, 
+                    epochs=10)
+    future = model.make_future_dataframe(prcp_data, periods=365, n_historic_predictions=len(df))
+    forecast = model.predict(future)
+
+    def plot_forecast(model, data, periods, historic_pred=True, highlight_steps_ahead=None):    
+        global forecast
+        future = model.make_future_dataframe(data, 
+                                         periods=periods, 
+                                         n_historic_predictions=historic_pred)
+        forecast = model.predict(future)
+    
+        if highlight_steps_ahead is not None:
+            model = model.highlight_nth_step_ahead_of_each_forecast(highlight_steps_ahead)
+            mbodel.plot_last_forecast(forecast)
+        else:    
+            model.plot(forecast)
+
+    st.subheader('Predictions vs Actual')
+    fig, st = plot.subplots(figsize=(14, 10))
+    plot_forecast(model, prcp_data, periods=60)
+    model.plot(forecast, xlabel="Date", ylabel="Price")
+    st.set_title("KOSPI INDEX", fontsize=28, fontweight="bold")
+    st.pyplot()
