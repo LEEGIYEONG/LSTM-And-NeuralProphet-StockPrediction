9847f9985ac1f85feac00cfed1b76bd3078023fa
LEEGIYEONG <dnjsrhr775@gmail.com>
2021-10-26 20:52:27 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from apps import first1, forth1, third1
 app = MultiApp()
 
 st.markdown("""
-# 메인 홈페이지
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+# 주가지수 예측 프로그램
 """)
 
 # Add all your application here
