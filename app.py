843e27a1ea69de31ffca92569eea455583cbc727
LEEGIYEONG <dnjsrhr775@gmail.com>
2021-11-22 22:14:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import main_home, LSTM_main#np_main
+from apps import main_home, LSTM_main,np_main
 
 app = MultiApp()
 
@@ -11,6 +11,6 @@ st.markdown("""
 # Add all your application here
 app.add_app("메인", main_home.app)
 app.add_app("LSTM", LSTM_main.app)
-#app.add_app("NeuralProphet", np_main.app)
+app.add_app("NeuralProphet", np_main.app)
 # The main app
 app.run()
