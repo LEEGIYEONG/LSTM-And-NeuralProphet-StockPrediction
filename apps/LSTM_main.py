0c185316b0a61ab902ee8e3c5554c77c89747eb1
LEEGIYEONG <dnjsrhr775@gmail.com>
2021-12-07 16:53:49 +0900

    Update LSTM_main.py

--- apps/LSTM_main.py
+++ apps/LSTM_main.py
@@ -1,17 +1,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import pandas_datareader as data
+import FinanceDataReader as fdr
 from keras.models import load_model
 from datetime import date
 import streamlit as st
 
 def app():
-    START = '2000-01-01'
-    TODAY = date.today().strftime('%Y-%m-%d')
-
-    user_input = st.text_input('Enter Stock Ticker', '^KS11')
-    df = data.DataReader(user_input,"yahoo", START, TODAY)
+    
+    df = fdr.DataReader('KS11', '2000')
+    
+    df_data = df.rename(columns={'Adj Close': '수정 종가', 'Volume': '변동율', 'High': '고가', 'Low': '저가', 'Open': '시가'
+                                 , 'Close': '종가'})
     
     st.subheader('코스피 주가지수 2000 - 2021')
     st.write(df.describe())
@@ -59,8 +59,6 @@ def app():
     scale_factor = 1/0.00042257
     y_predicted = y_predicted * scale_factor
     y_test = y_test * scale_factor
-
-
     
     st.subheader('주가 예측')
     st.write('예측 결과 : 파란색은 실제 주가지수를 의미, 빨간색은 예측 주가지수를 의미 ')
